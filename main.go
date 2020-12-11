23520135f7ccc18d13cf705ce71c0c7528f05d1e
JeongHyeon-VIRNECT <practice1356@gmail.com>
2020-12-11 21:49:33 +0900

    declare variable

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// 명시적이 변수 선언
+	var name string = "johnmark"
+	fmt.Println(name)
+
+	// 축약형 변수 선언, 타입추론을 통해 변수 자료형 선택
+	message := "hi!"
+	fmt.Println(message)
 }
